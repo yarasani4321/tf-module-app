812fa32ab90811d2c7627a72bd9555f44eccb684
Raghu K <raghuk.vit@gmail.com>
2023-08-28 06:33:09 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -101,6 +101,7 @@ resource "aws_lb_target_group" "public" {
   count = var.component == "frontend" ? 1 : 0
   name     = "${local.name_prefix}-public"
   port     = var.port
+  target_type = "ip"
   protocol = "HTTP"
   vpc_id   = var.vpc_id
 }
