317d908d38d9735e16f7186853e2363b21d1eb3e
Raghu K <raghuk.vit@gmail.com>
2023-08-29 07:25:58 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ resource "aws_iam_policy" "main" {
           "ssm:GetParameters",
           "ssm:GetParameter"
         ],
-        "Resource": "arn:aws:ssm:us-east-1:633788536644:parameter/docdb.${var.env}.*"
+        "Resource": local.policy_resources
       },
       {
         "Sid": "VisualEditor1",
