a3eabbf42fd7f0d11181e43d1834b48bc7482615
Raghu K <raghuk.vit@gmail.com>
2023-08-29 06:52:18 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -29,12 +29,73 @@ resource "aws_security_group" "main" {
   }
 }
 
+resource "aws_iam_policy" "main" {
+  name        = "${local.name_prefix}-policy"
+  path        = "/"
+  description = "${local.name_prefix}-policy"
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "VisualEditor0",
+        "Effect": "Allow",
+        "Action": [
+          "ssm:GetParameterHistory",
+          "ssm:GetParametersByPath",
+          "ssm:GetParameters",
+          "ssm:GetParameter"
+        ],
+        "Resource": "arn:aws:ssm:us-east-1:633788536644:parameter/docdb.${var.env}.*"
+      },
+      {
+        "Sid": "VisualEditor1",
+        "Effect": "Allow",
+        "Action": "ssm:DescribeParameters",
+        "Resource": "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "main" {
+  name = "${local.name_prefix}-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = merge(local.tags, { Name = "${local.name_prefix}-role" })
+}
+
+resource "aws_iam_role_policy_attachment" "attach" {
+  role       = aws_iam_role.main.name
+  policy_arn = aws_iam_policy.main.arn
+}
+
+resource "aws_iam_instance_profile" "main" {
+  name = "${local.name_prefix}-role"
+  role = aws_iam_role.main.name
+}
+
 resource "aws_launch_template" "main" {
   name                   = local.name_prefix
   image_id               = data.aws_ami.ami.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.main.id]
-  iam_instance_profile {}
+  iam_instance_profile {
+    name = "${local.name_prefix}-role"
+  }
 
   user_data = base64encode(templatefile("${path.module}/userdata.sh",
     {
@@ -132,64 +193,5 @@ resource "aws_lb_listener_rule" "public" {
       values = ["${var.env}.rdevopsb72.online"]
     }
   }
-}
-
-resource "aws_iam_policy" "main" {
-  name        = "${local.name_prefix}-policy"
-  path        = "/"
-  description = "${local.name_prefix}-policy"
-
-  policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Sid": "VisualEditor0",
-        "Effect": "Allow",
-        "Action": [
-          "ssm:GetParameterHistory",
-          "ssm:GetParametersByPath",
-          "ssm:GetParameters",
-          "ssm:GetParameter"
-        ],
-        "Resource": "arn:aws:ssm:us-east-1:633788536644:parameter/docdb.${var.env}.*"
-      },
-      {
-        "Sid": "VisualEditor1",
-        "Effect": "Allow",
-        "Action": "ssm:DescribeParameters",
-        "Resource": "*"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role" "main" {
-  name = "${local.name_prefix}-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
-
-  tags = merge(local.tags, { Name = "${local.name_prefix}-role" })
-}
-
-resource "aws_iam_role_policy_attachment" "attach" {
-  role       = aws_iam_role.main.name
-  policy_arn = aws_iam_policy.main.arn
-}
-
-resource "aws_iam_instance_profile" "main" {
-  name = "${local.name_prefix}-role"
-  role = aws_iam_role.main.name
 }
 
